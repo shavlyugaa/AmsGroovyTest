b05a36ad44820b181a777296077ae5c16a55ceb5
A.Shavlyuga <a.shavlyuga@itransition.com>
2017-05-16 18:07:58 +0300

    init

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,33 @@
+node ('test') {
+ 
+    // Mark the code checkout 'Checkout'....
+    stage 'Checkout'
+ 
+    // // Get some code from a GitHub repository
+    checkout scm
+
+    // Set environment variables
+    stage "Setenv"
+    // build parameters
+    properties properties: [[
+      $class                : 'ParametersDefinitionProperty',
+      parameterDefinitions  : [
+              [name         : 'APP',               
+               $class       : 'StringParameterDefinition'],
+              [name         : 'ENVIRONMENT',
+               defaultValue : 'staging',               
+               $class       : 'StringParameterDefinition'],
+              [name         : 'PACKER',              
+               $class       : 'StringParameterDefinition']
+      ]
+    ]]
+    
+    
+    env.APP = APP
+    env.ENVIRONMENT = ENVIRONMENT
+    env.PACKER = PACKER
+    
+    echo env.APP
+    echo env.ENVIRONMENT
+    echo env.PACKER
+}
